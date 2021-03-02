2d8b84449651516b325d7b5482c5ffa290feeeac
repl.it user <replituser@example.com>
2021-03-02 10:17:03 +0000

    Demos week 1

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-print ("Hello")
+print("Hello")
 
 print("What is your name?")
 #Variable is a container, which can store data for us in the memory (string, integer, float, bool)
 
 name = input()
 print(type(name))
-
 
 #---------------
 
@@ -19,4 +18,15 @@ balance = float(input())
 print(type(balance))
 
 #------------------
-print("Welcome {} you are said to be {} years old. Your bank balance is {} $.".format(name, age, balance))
+print("Welcome {} you are said to be {} years old. Your bank balance is {} $.".
+      format(name, age, balance))
+
+#==========================
+
+print("What is your name?")
+n = input()
+
+if len(n) > 9:
+    print("you have a very loong name")
+    print("Your name contains {} letters".format(len(n)))
+print("This is the end of the program")
