4a1ec837a43f11d9f9f19cf9afc96dd25fb2d7a1
repl.it user <replituser@example.com>
2021-03-02 10:40:04 +0000

    Week 1 demos

--- main.py
+++ main.py
@@ -29,4 +29,19 @@ n = input()
 if len(n) > 9:
     print("you have a very loong name")
     print("Your name contains {} letters".format(len(n)))
+else: 
+    print("your name is quite okay")
+    print("Your name contains {} letters".format(len(n)))
+print("This is the end of the program")
+
+#================================
+print("Do you have a dog ? ( Type True or False)")
+dog = bool(input())
+# bool is bolean datatype - on;y stores True/False
+if len(n) > 9 and dog:
+    print("you have a very loong name")
+    print("Your name contains {} letters".format(len(n)))
+else: 
+    print("your name is quite okay")
+    print("Your name contains {} letters".format(len(n)))
 print("This is the end of the program")
