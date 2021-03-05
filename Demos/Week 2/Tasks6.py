1c677b1c9fb31d88d237d8f1838c0a8521e3e8c6
repl.it user <replituser@example.com>
2021-03-05 11:27:27 +0000

    Week 2 tasks Rbots

--- Demos/Week 2/Tasks6.py
+++ Demos/Week 2/Tasks6.py
@@ -1,3 +1,4 @@
+
 #We wish to create a program that allows us to specify what type of book is being read by Beep.
 #The program should start by asking the user to enter the type of book e.g. adventure.
 #If the book is an "adventure" book then the message "I like adventure books!" should be displayed.
@@ -12,3 +13,35 @@ else:
 print("Finished reading book!")
 
 #=============================================
+#We wish to create a program that allows us to help Beep by suggesting activities for Beep to complete.
+#The program should start by asking the user to enter an activity.
+#If the activity is "calculate" then the message "Performing calculations..." should be displayed otherwise the message "Performing activity..." should be displayed.
+#Finally, the program should display "Activity completed!".
+
+print("Please enter the activity to be performed?")
+print("1 - Calculate")
+print("2 - Test")
+print("3 - Test")
+
+option = int(input())
+
+if option == 1:
+  print("Calculate")
+  print("Performing calculations...")
+  print("Activity completed!")
+
+#================================================
+#We wish to create a program that allows us to help Beep learn to paint.
+#The program should start by prompting the user for a directi on to move the paint brush (up, down, left or right).
+#The program should then work out in which direction Beep should paint and display a suitable message.
+
+print("Towards which direction should I paint (up, down, left or right?")
+direction = input()
+if direction == "up":
+   print("Go up")
+elif direction == "down": 
+    print("Go down")
+elif direction == "right":
+    print("Go right")
+elif direction == "left":
+    print("Go left")
