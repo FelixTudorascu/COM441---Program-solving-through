81f3b306927e238b64f0705e2a20a314fdd1e6ad
repl.it user <replituser@example.com>
2021-03-08 18:07:28 +0000

    Week 2 continue task

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print("Please enter a whole number.")
-number = int(input())
-if (number%2) == 0:
-    print("The number {} is an even number." .format(number))
-else: 
-    print("The number {} is an odd number." .format(number))
+print("Please enter the first number:")
+num1 = int(input())
+print("please enter the second number:")
+num2 = int(input())
+if num1>num2:
+  print("{} is bigger then {}" .format(num1,num2))
+elif num1<num2:
+  print("{} is smaller then {}" .format(num1,num2))
+elif num1 == num2:
+  print("{} is equal with {}" .format(num1,num2))
